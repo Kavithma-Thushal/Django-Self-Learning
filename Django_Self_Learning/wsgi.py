5b00042f0051010cf88447fe961272693f3d0818
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-10-03 22:28:08 +0530

    Initial commit

---
+++ Django_Self_Learning/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Django_Self_Learning project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Self_Learning.settings')
+
+application = get_wsgi_application()
