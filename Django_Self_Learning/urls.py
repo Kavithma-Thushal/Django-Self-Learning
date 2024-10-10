ce190949345ca31a9f3e5deb435bd10dc958a69f
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-10-10 13:38:48 +0530

    Code Refactored

--- Django_Self_Learning/urls.py
+++ Django_Self_Learning/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Backend project.
+URL configuration for Django_Self_Learning project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
